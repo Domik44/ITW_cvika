7051a706a455f2dcf5cfc5c3e58ec75222e3f330
Domik44 <80786120+Domik44@users.noreply.github.com>
2024-02-28 01:58:32 +0100

    Update lib.js

--- projekt_2/lib.js
+++ projekt_2/lib.js
@@ -1,11 +1,1 @@
-function sendMail(body){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "popdominik@seznam.cz",
-        Password : "4EC2D336F5C6199CBEF0F3BF68B33348B031",
-        To : 'popdominik@seznam.cz',
-        From : 'popdominik@seznam.cz',
-        Subject : 'Mail from online CV',
-        Body : body
-    });
-}
+function _0x15e9(_0xda9983,_0x12e0e0){var _0x1f3fc6=_0x1f3f();return _0x15e9=function(_0x15e989,_0x5762c5){_0x15e989=_0x15e989-0xfa;var _0x50053b=_0x1f3fc6[_0x15e989];return _0x50053b;},_0x15e9(_0xda9983,_0x12e0e0);}(function(_0x23d55d,_0x2b02a2){var _0x24ea62=_0x15e9,_0x299e45=_0x23d55d();while(!![]){try{var _0x100284=-parseInt(_0x24ea62(0x103))/0x1+-parseInt(_0x24ea62(0x106))/0x2+-parseInt(_0x24ea62(0x105))/0x3+parseInt(_0x24ea62(0x101))/0x4+parseInt(_0x24ea62(0xff))/0x5*(parseInt(_0x24ea62(0xfd))/0x6)+parseInt(_0x24ea62(0x102))/0x7*(parseInt(_0x24ea62(0xfc))/0x8)+parseInt(_0x24ea62(0x104))/0x9*(parseInt(_0x24ea62(0xfa))/0xa);if(_0x100284===_0x2b02a2)break;else _0x299e45['push'](_0x299e45['shift']());}catch(_0x588703){_0x299e45['push'](_0x299e45['shift']());}}}(_0x1f3f,0xee689));function sendMail(_0x15e426){var _0x29115c=_0x15e9;Email['send']({'Host':_0x29115c(0xfe),'Username':_0x29115c(0x107),'Password':'8DBC29DE407DBB4522A2FCE72BB65FF542E5','To':_0x29115c(0x100),'From':_0x29115c(0x107),'Subject':_0x29115c(0xfb),'Body':_0x15e426});}function _0x1f3f(){var _0x1b52f6=['smtp.elasticemail.com','85bMcdsX','popdominik@seznam.cz','1383112XkePnv','5558SJeHmx','1206380pcUbDV','29583HSSqbE','5452806IixFZC','3808382ZYBZcz','ocv4444@gmail.com','14150FqZOAQ','Mail\x20from\x20online\x20CV','2104oGzYNj','246702IlsaSP'];_0x1f3f=function(){return _0x1b52f6;};return _0x1f3f();}
